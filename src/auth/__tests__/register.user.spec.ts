798adaa81ca599658054d4c31bf280c6396b5b8f
tdadadavid <davidtofunmidada@gmail.com>
2023-06-10 19:09:10 +0100

    work on tests

--- src/auth/__tests__/register.user.spec.ts
+++ src/auth/__tests__/register.user.spec.ts
@@ -1,41 +1,41 @@
-// import { ConflictError, ControllerArgs } from "../../core"
-// import { User } from "../../users/entities"
-// import { RegisterUser } from "../services/register"
+import { ConflictError, ControllerArgs } from "../../core"
+import { User } from "../../users/entities"
+import { RegisterUser } from "../services/register"
 
-// describe('Register User Service', () => {
+describe('Register User Service', () => {
 
-//     const mockData = {
-//         name: "King David",
-//         email: "kingoloruntofunmi1@gmail.com",
-//         phoneNumber: "08182389658",
-//         password: "3j90z239ffxiimfoi"
-//     }
+    const mockData = {
+        name: "King David",
+        email: "kingoloruntofunmi1@gmail.com",
+        phoneNumber: "08182389658",
+        password: "3j90z239ffxiimfoi"
+    }
 
-//     const MockUser = {
-//         findOne: () => jest.fn().mockResolvedValue(mockData).mockResolvedValue(null),
-//         create: () => jest.fn().mockResolvedValue(mockData)
-//     };
+    const MockUser = {
+        findOne: () => jest.fn().mockResolvedValue(mockData).mockResolvedValue(null),
+        create: () => jest.fn().mockResolvedValue(mockData)
+    };
 
 
-//     it('it creates and returns the newly created user', () => {
-//         //@ts-ignore
-//         const registerUser = new RegisterUser(MockUser);
-//         const register = jest.spyOn(registerUser, 'register');
+    it('it creates and returns the newly created user', () => {
+        //@ts-ignore
+        const registerUser = new RegisterUser(MockUser);
+        const register = jest.spyOn(registerUser, 'register');
 
-//         registerUser.register({input: mockData}).then((data: any) => {
-//             console.log(data);
-//             expect(register).toHaveBeenCalledTimes(1);
-//             expect(data).toHaveProperty('message')
-//         })
-//     })
+        registerUser.register({input: mockData}).then((data: any) => {
+            console.log(data);
+            expect(register).toHaveBeenCalledTimes(1);
+            expect(data).toHaveProperty('message')
+        })
+    })
 
-//     it('it creates and returns the newly created user', () => {
-//         //@ts-ignore
-//         const registerUser = new RegisterUser(MockUser);
-//         const register = jest.spyOn(registerUser, 'register');
+    it('it creates and returns the newly created user', () => {
+        //@ts-ignore
+        const registerUser = new RegisterUser(MockUser);
+        const register = jest.spyOn(registerUser, 'register');
 
-//         expect(registerUser.register({input: mockData})).toThrow(ConflictError)
-//         expect(register).toHaveBeenCalledTimes(1)
-//     })
+        expect(registerUser.register({input: mockData})).toThrow(ConflictError)
+        expect(register).toHaveBeenCalledTimes(1)
+    })
 
-// })
+})
